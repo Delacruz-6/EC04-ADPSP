f530dac8bce5ab67fa710c02fe0933d351d9db64
Delacruz-6 <gdlcruzguzman@gmail.com>
2021-09-29 16:35:56 +0200

    Añadidas clases repository

---
+++ EC04J1/src/main/java/com/salesianostriana/dam/EC04J1/DireccionRepository.java
@@ -0,0 +1,7 @@
+package com.salesianostriana.dam.EC04J1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DireccionRepository
+        extends JpaRepository<Direccion, Long> {
+}
