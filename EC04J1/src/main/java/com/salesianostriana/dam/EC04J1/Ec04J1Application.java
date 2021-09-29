ea6e835449857d9cd4e38dd32c57b51a4acf8ca0
Delacruz-6 <gdlcruzguzman@gmail.com>
2021-09-29 15:36:34 +0200

    Ejercicio-1 entidades

---
+++ EC04J1/src/main/java/com/salesianostriana/dam/EC04J1/Ec04J1Application.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.EC04J1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec04J1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec04J1Application.class, args);
+	}
+
+}
---
+++ EC04J1/src/test/java/com/salesianostriana/dam/EC04J1/Ec04J1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.EC04J1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec04J1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
